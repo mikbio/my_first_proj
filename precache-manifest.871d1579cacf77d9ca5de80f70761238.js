f2ff53e108ca694f9cc15c76da2de3589a30e776
mirko biondi <mirkobiondi@MBP-di-mirko.homenet.telecomitalia.it>
2020-04-19 12:01:22 +0200

    Updates

---
+++ precache-manifest.871d1579cacf77d9ca5de80f70761238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a44102377f56738e9c2fc2d5da8a6bd",
+    "url": "/mikbio/my_first_proj/index.html"
+  },
+  {
+    "revision": "a620d82dcb986284c77e",
+    "url": "/mikbio/my_first_proj/static/css/main.097df4f6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mikbio/my_first_proj/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mikbio/my_first_proj/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a620d82dcb986284c77e",
+    "url": "/mikbio/my_first_proj/static/js/main.d2384210.chunk.js"
+  },
+  {
+    "revision": "b6815148cdda0900737e",
+    "url": "/mikbio/my_first_proj/static/js/runtime-main.c17d86dc.js"
+  }
+]);
